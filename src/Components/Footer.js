7bbe1369802ef4ef4f662940a35a6f72c1c5cda3
Manos Gouvrikos <manolisgouvrikos@gmail.com>
2020-03-02 04:10:38 +0200

    Footer is now fixed on the bottom of the screen

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -6,8 +6,9 @@ import { Container, Row, Col } from 'react-bootstrap';
 function Footer() {
     let currentYear = new Date().getFullYear();
     return (
-        <footer id="copyright" className="mt-5">
-            <Container fluid={true}>
+        <div className="footer">
+        <footer className="mt-5" >
+            <Container id="container"fluid={true} >
                 <Row className="border-top justify-content-between p-3">
 
                     <Col className="p-0 d-flex justify-content" md={3}>
@@ -16,6 +17,7 @@ function Footer() {
                 </Row>
             </Container>
         </footer>
+        </div>
     );
 
 }
